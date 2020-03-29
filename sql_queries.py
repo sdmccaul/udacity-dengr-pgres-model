9fa544056973b96470f62e9a8b82aed59863eac8
Steven McCauley <steven_mccauley@brown.edu>
2020-03-29 10:07:26 -0400

    ETL process completes

--- sql_queries.py
+++ sql_queries.py
@@ -96,31 +96,41 @@ CREATE TABLE IF NOT EXISTS songplays(
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO 
+INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+INSERT INTO users(user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE set level = EXCLUDED.level;
 """)
 
 song_table_insert = ("""
 INSERT INTO songs(song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES (%s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-VALUES (%s, %s, %s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT s.song_id, a.artist_id
+FROM songs as s, artists as a
+WHERE s.artist_id = a.artist_id and s.title=%s and a.name = %s and s.duration = %s;
 """)
 
 # QUERY LISTS
